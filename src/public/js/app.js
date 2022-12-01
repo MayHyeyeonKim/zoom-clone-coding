9356cacfe3a22f98b06d0a12629382cbf97add17
MayHyeyeonKim <hkim7963@gmail.com>
2022-12-01 00:32:41 -0800

    Room Count part Two

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -21,9 +21,19 @@ function handleMessageSubmit(event) {
   const value = input.value;
   socket.emit("new_message", input.value, roomName, () => {
     addMessage(`You: ${value}`);
-  });
+  }); 
   input.value = "";
 }
+
+function handleNameSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("#name input");
+  const value = input.value;
+  socket.emit("nickname", input.value);
+  input.value = "";
+}
+
+
 
 function showRoom() {
   welcome.hidden = true;
@@ -32,6 +42,9 @@ function showRoom() {
   h3.innerText = `Room ${roomName}`;
   const msgForm = room.querySelector("#msg");
   msgForm.addEventListener("submit", handleMessageSubmit);
+
+  const nameForm = room.querySelector("#name");
+  nameForm.addEventListener("submit", handleNameSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -44,12 +57,19 @@ function handleRoomSubmit(event) {
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", (user) => {
+socket.on("welcome", (user, newCount) => {
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName} (${newCount})`;
   addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", (left) => {
+socket.on("bye", (left, newCount) => {
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName} (${newCount})`;
   addMessage(`${left} left ㅠㅠ`);
 });
 
 socket.on("new_message", addMessage);
+
+
+socket.on("room_change", console.log);
